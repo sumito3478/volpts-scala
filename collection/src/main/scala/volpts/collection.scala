80c0686b6e7a2befad8436de73d81286365d0b00
sumito3478 <sumito3478@gmail.com>
2013-05-12 00:07:41 +0900

    Remove IndexedSeqOptimized from super types of collection.StringView

--- collection/src/main/scala/volpts/collection.scala
+++ collection/src/main/scala/volpts/collection.scala
@@ -96,7 +96,7 @@ package object collection {
     override def empty = new MutableArrayMap[A](size)
   }
 
-  case class StringView(src: String, from: Int, until: Int) extends IndexedSeq[Char] with IndexedSeqOptimized[Char, StringView] {
+  case class StringView(src: String, from: Int, until: Int) extends IndexedSeq[Char] {
     def apply(idx: Int) = src.charAt(from + idx)
 
     def length: Int = until - from
