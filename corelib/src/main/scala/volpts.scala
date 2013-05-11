e2d38f11d04de522ed80d487cf7a3c238cad3c96
sumito3478 <sumito3478@gmail.com>
2013-05-12 00:08:37 +0900

    Add volpts.__, ---, Dummy0 to Dummy3

--- corelib/src/main/scala/volpts.scala
+++ corelib/src/main/scala/volpts.scala
@@ -1,3 +1,15 @@
 package object volpts {
   type -->[-A, +B] = PartialFunction[A, B]
+
+  class __
+
+  class ---[A, B]
+
+  implicit object Dummy0 extends (__ --- __)
+
+  implicit object Dummy1 extends (__ --- __ --- __)
+
+  implicit object Dummy2 extends (__ --- __ --- __ --- __)
+
+  implicit object Dummy3 extends (__ --- __ --- __ --- __ --- __)
 }
