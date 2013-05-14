9835f2f41dd0979b9fa237e29f0ec16fc2443c42
sumito3478 <sumito3478@gmail.com>
2013-05-15 03:03:18 +0900

    Add support for "lazy val name = <macro valDefWithName>(something)"

--- meta/src/main/scala/volpts/meta.scala
+++ meta/src/main/scala/volpts/meta.scala
@@ -16,17 +16,21 @@ package object meta {
   def valDefWithName[A: c.WeakTypeTag](c: Context)(valWithName: c.Expr[ValWithName[A]]): c.Expr[A] = {
     import c.universe._
     val name = c.Expr[String](Literal(Constant((for {
-      trees <- c.toCompilerContext.callsiteTyper.context.enclosingContextChain.map(_.tree.asInstanceOf[c.Tree]).tails
-      name <- trees match {
+      treeTail <-  (for {
+        chain <- c.toCompilerContext.callsiteTyper.context.enclosingContextChain
+        tree = chain.tree.asInstanceOf[c.Tree]
+      } yield(tree)).tails
+      name <- treeTail match {
         case ValDef(_, name, _, _) :: _ => Some(name.decoded.trim)
         case (_: Apply | _: Select | _: TypeApply) :: _ => None
-        case Block(_, _) :: DefDef(mods, name, _, _, _, _) :: _
-          if mods.hasFlag(Flag.LAZY) => Some(name.decoded.trim)
+        case Block(_, _) :: DefDef(mods, name, _, _, _, _) :: _ if mods.hasFlag(Flag.LAZY) => Some(name.decoded.trim)
+        case DefDef(mods, name, _, _, _, _) :: _ if mods.hasFlag(Flag.LAZY) => Some(name.decoded.trim)
         case _ => {
-          c.error(c.enclosingPosition, s"expected: val someName = ${c.macroApplication.symbol.name.decoded}[SomeType](someTypeInstance)")
+          c.error(c.enclosingPosition, s"expected: [lazy] val someName = ${c.macroApplication.symbol.name.decoded}[SomeType](someTypeInstance)")
           Some("<error>")
         }
-      }} yield (name)).next)))
+      }
+    } yield(name)).next)))
     reify(valWithName.splice(name.splice))
   }
 }
